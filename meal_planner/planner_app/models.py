1db9d829aa16f1cc60fae99c3aed166854f61c09
Veturna <dpullo@outlook.com>
2023-02-11 17:00:45 +0100

    create models

--- meal_planner/planner_app/models.py
+++ meal_planner/planner_app/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Product:
+    name = models.CharField(max_length=64, unique=True)
+    category = models.CharField(null=False)
+
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+    description = models.TextField(null=True)
+    products = models.ManyToManyField(Product, through='ProductInRecipe')
+    preparation = models.TextField(null=False)
+
+
+class ProductInRecipe(models.Model):
+    product = models.ForeignKey(Product)
+    recipe = models.ForeignKey(Recipe)
+    quantity = models.IntegerField()
+
+
+class Plan(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+    description = models.TextField(null=True)
+    date = models.DateTimeField()
+    recipes = models.ManyToManyField(Recipe)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+class ShoppingList(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+    date = models.DateTimeField()
+    products = models.ManyToManyField(Product)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
