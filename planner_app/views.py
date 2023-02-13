385b3b445e8c0387dab64e6f486ead5abd8d004a
Veturna <dpullo@outlook.com>
2023-02-13 19:37:22 +0100

    add some views

--- planner_app/views.py
+++ planner_app/views.py
@@ -11,3 +11,36 @@ class AboutApp(View):
     def get(self, request):
         return render(request, "about_app.html")
 
+class Login(View):
+    """Widok logowania"""
+    pass
+
+
+class UserView(View):
+    """Widok po zalogowaniu"""
+    pass
+
+
+class EditUser(View):
+    """Edycja użytkownika"""
+    pass
+
+
+class PlansView(View):
+    """Widok planów"""
+    pass
+
+
+class RecipesView(View):
+    """Widok przepisów"""
+    pass
+
+
+class AddRecipe(View):
+    """Dodanie przepisu"""
+    pass
+
+
+class EditRecipe(View):
+    """Edycja przepisu"""
+    pass
