2a03df7675cc66ee9a0f75a0d61afa500b3624aa
Veturna <dpullo@outlook.com>
2023-02-20 17:12:43 +0100

    debbuging

--- planner_app/form.py
+++ planner_app/form.py
@@ -1,7 +1,11 @@
 from django import forms
+from planner_app.models import QUANTITY_CATEGORIES
 
 
 class EditRecipeForm(forms.Form):
     name = forms.CharField(label="Nazwa", max_length=64)
     description = forms.CharField(max_length=2000)
     preparation = forms.CharField(max_length=10000, widget=forms.Textarea())
+    product = forms.CharField(max_length=255)
+    quantity = forms.IntegerField()
+    quantity_categories = forms.ChoiceField(choices=QUANTITY_CATEGORIES)
