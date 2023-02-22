96c539a4c3167e1f9c710cfed239adb5fa627366
Veturna <dpullo@outlook.com>
2023-02-22 18:25:06 +0100

    edit the templates

--- meal_planner/urls.py
+++ meal_planner/urls.py
@@ -20,7 +20,7 @@ from planner_app.views import MainPage, AboutApp, RecipesView, RecipeDetail, Edi
 
 
 urlpatterns = [
-    path('accounts/', include('django.contrib.auth.urls', 'auth', namespace='accounts')),
+    path('accounts/', include(('django.contrib.auth.urls', 'auth'), namespace='accounts')),
     path('admin/', admin.site.urls),
     path('', MainPage.as_view(), name='main-page'),
     path('about/', AboutApp.as_view(), name='about-app'),
