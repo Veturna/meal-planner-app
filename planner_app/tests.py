531613e3258b0f949b6b73bbd2026587ad52ceaa
Veturna <dpullo@outlook.com>
2023-03-02 09:59:18 +0100

    add test to main page and about app page

--- planner_app/tests.py
+++ planner_app/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+import pytest
 
-# Create your tests here.
+@pytest.fixture
+def client():
+    client = Client()
+    return client
+
+
+def test_main_page(client):
+    response = client.get('')
+    assert response.status_code == 200
+
+
+def test_about_app(client):
+    response = client.get('about/')
+    assert response.status_code == response
+
